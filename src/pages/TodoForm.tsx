b064c3a26ed7e790327d0ed5c2eb33093bcc69d3
Abhishek lodha <119124845+Abhisheklodha01@users.noreply.github.com>
2025-01-26 03:43:46 +0530

    created add todo form

--- src/pages/TodoForm.tsx
+++ src/pages/TodoForm.tsx
@@ -1,7 +1,113 @@
-import React from 'react'
+import React, { useState } from "react";
+import { cn } from "@/lib/utils";
+import axios from "axios";
+import { backendUrl } from "@/utils/server";
+import { toast } from "react-hot-toast";
+import { Label } from "@radix-ui/react-label";
+import { Input } from "@/components/ui/input";
 
 export const TodoForm = () => {
+  const [name, setName] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post(
+        `${backendUrl}/todos/add-todo`,
+        {
+          name,
+          description,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+        }
+      );
+      toast.success(data.message, {
+        position: "top-center",
+      });
+    } catch (error: any) {
+      toast.error(error.response.data.message, {
+        position: "top-center",
+      });
+    }
+  };
+
   return (
-    <div>TodoForm</div>
-  )
-}
+    <div className="min-h-screen dark">
+      <div className="pb-40"></div>
+      <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black border border-slate-700">
+        <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
+          Welcome to API Stack
+        </h2>
+        <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
+          Add Todos Data!
+        </p>
+
+        <form className="my-8" onSubmit={handleSubmit}>
+          <LabelInputContainer className="mb-4">
+            <Label htmlFor="name">Todo Name</Label>
+            <Input
+              name="name"
+              id="name"
+              placeholder="enter todo name"
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </LabelInputContainer>
+          <LabelInputContainer className="mb-4">
+            <Label htmlFor="description">Todo Description</Label>
+            <Input
+              name="description"
+              id="description"
+              placeholder="enter todo description"
+              type="text"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+          </LabelInputContainer>
+          <button
+            className="bg-gradient-to-br relative group/btn from-black
+             dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block
+              dark:bg-zinc-800 w-full text-white rounded-md h-10 
+              font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset]
+               dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+            type="submit"
+          >
+            Add Todo &rarr;
+            <BottomGradient />
+          </button>
+
+          <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+const BottomGradient = () => {
+  return (
+    <>
+      <span className="group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-cyan-500 to-transparent" />
+      <span className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-500 to-transparent" />
+    </>
+  );
+};
+
+const LabelInputContainer = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div className={cn("flex flex-col space-y-2 w-full", className)}>
+      {children}
+    </div>
+  );
+};
