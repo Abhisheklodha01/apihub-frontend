c4baf8371f066546c1a61bce25aaf6a91b012dbd
Abhishek lodha <119124845+Abhisheklodha01@users.noreply.github.com>
2025-01-31 03:05:04 +0530

    added authentication in navabr

--- src/modules/NavBar.tsx
+++ src/modules/NavBar.tsx
@@ -1,6 +1,9 @@
+import { userContex } from "@/context/userContex";
+import React from "react";
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
+  const { isAuthenticated } = React.useContext(userContex);
   return (
     <div
       className="dark bg-black flex justify-between items-center
@@ -9,11 +12,19 @@ const NavBar = () => {
       <Link className="text-2xl font-bold text-purple-500" to={"/"}>
         APIStack
       </Link>
-      <Link to={"/auth/signup"}>
-      <button className="px-8 py-2 rounded-full bg-gradient-to-b from-blue-500 to-blue-600 text-white focus:ring-2 focus:ring-blue-400 hover:shadow-xl transition duration-200">
-  Signup
-</button>
-      </Link>
+      {isAuthenticated ? (
+        <Link to={"/user-profile"}>
+          <button className="px-8 py-2 rounded-full bg-gradient-to-b from-blue-500 to-blue-600 text-white focus:ring-2 focus:ring-blue-400 hover:shadow-xl transition duration-200">
+            Profile
+          </button>
+        </Link>
+      ) : (
+        <Link to={"/auth/signup"}>
+          <button className="px-8 py-2 rounded-full bg-gradient-to-b from-blue-500 to-blue-600 text-white focus:ring-2 focus:ring-blue-400 hover:shadow-xl transition duration-200">
+            Signup
+          </button>
+        </Link>
+      )}
     </div>
   );
 };
