90921313c3558844207824bf6bca65df8d05ecd1
Abhishek lodha <119124845+Abhisheklodha01@users.noreply.github.com>
2025-04-28 15:46:21 +0530

    chnaged backend url

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -1,2 +1,2 @@
-export const backendUrl = "https://apistack.onrender.com/api/v1"
+export const backendUrl = "https://api.apistack.site/api/v1"
 // export const backendUrl = "http://localhost:8000/api/v1"
